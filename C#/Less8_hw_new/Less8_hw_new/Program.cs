67f03d9c484cc9650d2bdfa9697ed0a32356ce45
Yurii Matusevich <matusevichy@gmail.com>
2021-06-22 20:20:39 +0300

    Add less9

---
+++ C#/Less8_hw_new/Less8_hw_new/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Less8_hw_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App(10);
+            app.Start();
+            Console.ReadKey();
+        }
+
+    }
+}
