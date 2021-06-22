99f949554d28908cb58daea899ab56c455e3a7cd
Yurii Matusevich <matusevichy@gmail.com>
2021-06-22 20:24:39 +0300

    tmp

--- C#/Less8_hw/Less8_hw/Program.cs
+++ C#/Less8_hw/Less8_hw/Program.cs
@@ -10,18 +10,18 @@ namespace Less8_hw
     {
         static void Main(string[] args)
         {
-//            System.Timers.Timer timer = new System.Timers.Timer { Interval = 1000 };
-//            timer.Elapsed += Timer_Tick;
-//            timer.Start();
+            System.Timers.Timer timer = new System.Timers.Timer { Interval = 1000 };
+            timer.Elapsed += Timer_Tick;
+            timer.Start();
 
             App app = new App(10);
             app.Start();
         }
 
-        //private static void Timer_Tick(object sender, EventArgs e)
-        //{
-        //    // Timer timer = sender as Timer;
-        //    Console.WriteLine("Tick");
-        //}
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            // Timer timer = sender as Timer;
+            Console.WriteLine("Tick");
+        }
     }
 }
