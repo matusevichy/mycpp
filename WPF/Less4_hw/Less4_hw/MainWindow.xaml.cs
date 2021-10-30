8d952d3c4db47441520e13b14fe05a0207dabaef
Yurii Matusevich <matusevichy@gmail.com>
2021-10-30 22:15:41 +0300

    new hw

--- WPF/Less4_hw/Less4_hw/MainWindow.xaml.cs
+++ WPF/Less4_hw/Less4_hw/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Less4_hw.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,32 @@ namespace Less4_hw
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Abonent> Abonents { get; set; } = new ObservableCollection<Abonent>
+        {
+            new Abonent
+            {
+                Id=1,
+                FirstName="Name 1",
+                MiddleName="N1",
+                LastName="Lastname 1",
+                Phone="11111111",
+                Address="Address 1"
+            },
+                        new Abonent
+            {
+                Id=2,
+                FirstName="Name 2",
+                MiddleName="N2",
+                LastName="Lastname 2",
+                Phone="22222222",
+                Address="Address 2"
+            }
+
+        };
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
---
+++ WPF/Less4_hw/Less4_hw/Models/Abonent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less4_hw.Models
+{
+    public class Abonent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
